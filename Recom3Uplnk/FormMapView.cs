6f226582d37a18f4277f57ec5e6e67bb5e7f3103
J Campos <jcampos73@vodafone.es>
2026-02-14 20:14:56 +0100

    more files

---
+++ Recom3Uplnk/FormMapView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Recom3Uplnk
+{
+    public partial class FormMapView : Form
+    {
+        public FormMapView()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMapView_Shown(object sender, EventArgs e)
+        {
+            this.Text = "Uplink Map Viewer";
+            this.Icon = Properties.Resources.ic_launcher;
+        }
+    }
+}
---
+++ Recom3Uplnk/UsbManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recom3Uplnk
+{
+    class UsbManager
+    {
+        public class USBDeviceInfo
+        {
+            public USBDeviceInfo(string deviceID, string pnpDeviceID, string description)
+            {
+                this.DeviceID = deviceID;
+                this.PnpDeviceID = pnpDeviceID;
+                this.Description = description;
+            }
+            public string DeviceID { get; private set; }
+            public string PnpDeviceID { get; private set; }
+            public string Description { get; private set; }
+        }
+
+        public static List<USBDeviceInfo> GetUSBDevices()
+        {
+            List<USBDeviceInfo> devices = new List<USBDeviceInfo>();
+
+            ManagementObjectCollection collection;
+            using (var searcher = new ManagementObjectSearcher(@"Select * From Win32_USBHub"))
+                collection = searcher.Get();
+
+            foreach (var device in collection)
+            {
+                devices.Add(new USBDeviceInfo(
+                (string)device.GetPropertyValue("DeviceID"),
+                (string)device.GetPropertyValue("PNPDeviceID"),
+                (string)device.GetPropertyValue("Description")
+                ));
+            }
+
+            collection.Dispose();
+            return devices;
+        }
+    }
+}
